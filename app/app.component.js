2b5feacbb0982d8a374221acc61994d1db672225
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2015-12-27 09:26:08 +0700

    add class for selected hero

--- app/app.component.js
+++ app/app.component.js
@@ -41,8 +41,8 @@
       template: `<h1>{{title}}</h1>          
          <h2>My Heroes</h2>
          <ul class="heroes">
-           <li *ngFor="#hero of heroes" (click)="onSelect(hero)">
-             <span class="badge">{{hero.id}}</span> {{hero.name}}
+           <li *ngFor="#hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+             <span class="badge" >{{hero.id}}</span> {{hero.name}}
            </li>
          </ul>
          <div *ngIf="selectedHero">
