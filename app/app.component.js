7580a69ecc2a2496b5d8430af14135096f72a93e
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2015-12-27 08:55:30 +0700

    change app.component.js
    content like https://angular.io/docs/ts/latest/tutorial/toh-pt1.html

--- app/app.component.js
+++ app/app.component.js
@@ -1,12 +1,30 @@
 (function(app) {
+
+  function Hero(hero) {
+    this.id = hero.id;
+    this.name = hero.name;
+  }
+
   app.AppComponent = ng.core
     .Component({
       selector: 'my-app',
-      template: '<h1>First App</h1>'
+      template: 
+        `<h1>{{title}}</h1>
+         <h2>{{hero.name}} details</h2>
+         <div><label>id: </label>{{hero.id}}</div> 
+         <div>
+            <label>name: </label>
+            <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+         </div>`
     })
     .Class({
-      constructor: function() {
 
+      constructor: function() {
+        this.title = "Tour of Heroes";
+        this.hero = new Hero({
+          id: 1,
+          name: 'Windstorm'
+        });
       }
     });
 })(window.app || (window.app = {}))
